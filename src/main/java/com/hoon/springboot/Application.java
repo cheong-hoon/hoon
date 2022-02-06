ddacaeadce02918aacb35c09c93d3a49f80f05e6
jhoon <jhoonyoo11@naver.com>
2022-02-06 20:24:34 +0900

    깃 테스트

--- src/main/java/com/hoon/springboot/Application.java
+++ src/main/java/com/hoon/springboot/Application.java
@@ -1,4 +1,11 @@
 package com.hoon.springboot;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
 
+@SpringBootApplication  //최상단에 위치해야함 자동설정 기능
 public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+    
 }
